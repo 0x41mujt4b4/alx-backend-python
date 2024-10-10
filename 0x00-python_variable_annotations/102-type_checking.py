5bb318292a8c3a7f8a5c71330d111a98befaa394
Almujtaba Abdalla <almujtaba.abdu@gmail.com>
2024-10-10 16:40:32 +0300

    update type checking function

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,13 +3,13 @@
 from typing import List, Tuple, Union
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Return a tuple of the list zoomed in by a factor"""
     zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
 array = (12, 72, 91)
