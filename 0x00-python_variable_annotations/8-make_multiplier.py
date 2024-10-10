9ff379e1d1166d40c06698b081af06e14b635109
Almujtaba Abdalla <almujtaba.abdu@gmail.com>
2024-10-10 16:03:35 +0300

    update make multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Type annotation module of make_multiplier function"""
+from typing import Callable, Union
 
 
-def make_multiplier(multiplier: float) -> callable:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a function that multiplies a float by multiplier"""
     def multiply(n: float) -> float:
         """Return the product of n and multiplier"""
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Type annotation module of element_length function"""
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """Return a list of tuples containing the string and its length"""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return a list of tuples containing the sequence and its length"""
     return [(i, len(i)) for i in lst]
