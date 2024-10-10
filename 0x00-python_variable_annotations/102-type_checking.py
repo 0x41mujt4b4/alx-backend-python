203a62908de107e8c8757f770ff1f7688c43c0c5
Almujtaba Abdalla <almujtaba.abdu@gmail.com>
2024-10-10 15:21:39 +0300

    add 0x00_python_variable_annotations

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple, Union
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return tuple(zoomed_in)
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,4 @@
+def make_multiplier(multiplier: float) -> callable:
+    def multiply(n: float) -> float:
+        return n * multiplier
+    return multiply
